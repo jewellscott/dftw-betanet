ec6b6c43672a295a686b495c34cb92d7bc8cf075
Jewell Scott <jewelljrscott@gmail.com>
2024-05-16 19:55:40 -0400

    Add Stephanie’s bitmojis to Friends and update randomizer

--- js/random-bitmoji.js
+++ js/random-bitmoji.js
@@ -2,5 +2,8 @@ function getRandomInt(max) { // number between 1 & max
   return (Math.floor(Math.random() * Math.floor(max)))+1;
 }
     
-let img = document.getElementById("random-bitmoji");
-img.src = "../jewell-hosted/bitmoji-resized/numbered/bitmoji" + getRandomInt(174) + ".png";
+let jewell = document.getElementById("random-bitmoji");
+jewell.src = "../jewell-hosted/bitmoji-resized/numbered/bitmoji" + getRandomInt(174) + ".png";
+
+let stephanie = document.getElementById("stephanie-bitmoji");
+stephanie.src = "../../jewell-hosted/bitmoji/stephanie/" + getRandomInt(12) + ".png";
