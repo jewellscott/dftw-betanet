fe89f34950b0952d931e7ff10005fc51763602b2
Jewell Scott <jewelljrscott@gmail.com>
2024-06-16 17:35:23 -0400

    Remove partials and fix missing pages

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<html lang="en">
+<!DOCTYPE html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +8,8 @@
 	<meta property="og:description" content="Jewell's PE Portal">
 	<meta property="og:image" content="https://peprojects.dev/beta-one/jewell/assets/img/social/rainbowskygrid.jpg">
 
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="./css/reset.css">
+	<link rel="stylesheet" href="./css/main.css">
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -30,13 +30,43 @@
 				<li><a href="./about/">About</a></li>
 				<li><a href="./blog/">Blog</a></li>
 				<li><a href="./work/">Work</a></li>
-				<li><a href="./inspo">Inspo</a></li>
+				<!-- <li><a href="./inspo">Inspo</a></li> -->
 				<li><a href="./goals/">Goals</a></li>
 				<li><a href="./links/">Links</a></li>
-				<li><a href="./extras">Extras</a></li>
+				<!-- <li><a href="./extras">Extras</a></li> -->
 				<li><a href="./friends/">Friends</a></li>
 			</ul>
 		</nav>
 	</header>
 
 	<main class="home-main page-main">
+		<div class="content-centered">
+			<div class="left">
+				<img src="/" id="random-bitmoji" class="home-bitmoji" alt="A bitmoji of me!">
+			</div>
+			<div class="right">
+				<h1>Hey! I'm Jewell.</h1>
+				<h2>Currently immersing myself in Design for the Web at Perpetual Education.</h2>
+				
+<!-- 				<p>Welcome to my crib! Take a <a href="./work/">look</a> around. Catch a <a href="./inspo">vibe</a>. Maybe even <a href="./extras#playlist">dance</a>.</p>
+				<span class="tech"><a href="./todo.markdown">TODO</a></span>
+				<span class="tech"><a href="./this-week.markdown">5/13 - 5/19</a></span> -->
+
+			</div>
+		</div>
+	</main>
+
+	<footer class="site-foot">
+		<p class="tagline"><a href="https://youtu.be/R_nviNS0IAw?si=G_oLg3MDTul68Zj5&t=205" target="_blank">Poor, lonely computer. It's time someone programmed you.</a></p>
+		<nav class="contact-nav">
+			<ul>
+				<li><a href="/">Contact</a></li>
+				<span class="footer-icon"> / </span>
+				<li><a href="/">Substack</a></li>
+			</ul>
+		</nav>
+	</footer>
+</div>
+<script src="./js/random-bitmoji.js"></script>
+</body>
+</html>
