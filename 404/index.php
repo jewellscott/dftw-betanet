b196440251f7f82025748f328f159241c375c43b
Jewell Scott <jewelljrscott@gmail.com>
2024-07-28 18:52:56 -0400

    Add 404 page

---
+++ 404/index.php
@@ -0,0 +1,12 @@
+<?php include '../templates/partials/header.php'; ?>
+
+<main class="home-main horizontal-center">
+
+	<div class="inner-column">
+
+		<?php includeFile('templates/modules/404-diptych.php')?>
+
+	</div>
+</main>
+
+<?php includeFile('templates/partials/footer.php'); ?>
