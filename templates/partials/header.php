6a1b6705b373eaaed37c90deefc0360f40f06439
Jewell Scott <jewelljrscott@gmail.com>
2024-07-28 12:22:23 -0400

    Remove forward slashes

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -11,7 +11,7 @@ require_once $baseDir . '/functions.php';
 	<meta charset="UTF-8">
 	<base href="<?=BASE_URL?>"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>✨ jewell.NEW ✨</title>
+	<title>✨ jewell.online ✨</title>
 	<meta name="Description" content="Jewell's PE Portal">
 	<meta property="og:title" content="JEWELL!">
 	<meta property="og:description" content="Jewell's PE Portal">
@@ -24,10 +24,10 @@ require_once $baseDir . '/functions.php';
 	<link href="https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,100..900;1,100..900&family=Inter:wght@100..900&family=Jersey+10&family=Jersey+15&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=VT323&display=swap" rel="stylesheet">
 
 
-	<link rel="icon" href="/assets/favicon/favicon.ico" sizes="32x32">
-	<link rel="icon" href="/assets/favicon/icon.svg" type="image/svg+xml">
-	<link rel="apple-touch-icon" href="/assets/favicon/apple-touch-icon.png"><!-- 180×180 -->
-	<link rel="manifest" href="/assets/favicon/manifest.webmanifest">
+	<link rel="icon" href="assets/favicon/favicon.ico" sizes="32x32">
+	<link rel="icon" href="assets/favicon/icon.svg" type="image/svg+xml">
+	<link rel="apple-touch-icon" href="assets/favicon/apple-touch-icon.png"><!-- 180×180 -->
+	<link rel="manifest" href="assets/favicon/manifest.webmanifest">
 </head>
 <body>
 
@@ -37,16 +37,16 @@ require_once $baseDir . '/functions.php';
 				<?php include(getFile('templates/components/site-logo.php'));?>
 			</a>
 			<ol class="link-list">
-				<li><a href="/about">About</a></li>
-				<li><a href="/blog">Blog</a></li>
-				<li><a href="/work">Work</a></li>
+				<li><a href="about">About</a></li>
+				<li><a href="blog">Blog</a></li>
+				<li><a href="work">Work</a></li>
 				<!-- <li><a href="./inspo">Inspo</a></li> -->
-				<li><a href="/goals">Goals</a></li>
-				<li><a href="/links">Links</a></li>
+				<li><a href="goals">Goals</a></li>
+				<li><a href="links">Links</a></li>
 				<!-- <li><a href="./extras">Extras</a></li> -->
-				<li><a href="/friends">Friends</a>
+				<li><a href="friends">Friends</a>
 				</li>
-				<li><a href="/style-guide">Style Guide</a></li>
+				<li><a href="style-guide">Style Guide</a></li>
 			</ol>
 		</nav>
 	</header>
