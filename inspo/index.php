edbd2f2a6ce956e1e4852b3edb0b9fc105220ea7
Jewell Scott <jewelljrscott@gmail.com>
2024-07-29 13:44:55 -0400

    Add inspo page

--- inspo/index.php
+++ inspo/index.php
@@ -1,3 +1,36 @@
-<?php include '../templates/partials/header.php'; ?>
+<?php include '../templates/partials/header.php';
 
-	<?php includeFile('templates/partials/footer.php'); ?>
+echo "<main>";
+
+// Get the directory of inspo images
+
+
+function getInspo() {
+
+	$colorInspo = scandir('inspo/colors');
+	array_splice($colorInspo, 0, 3);
+
+	// Loop through the contents
+
+	echo "<ul class='inspo-pics'>";
+
+	foreach($colorInspo as $image) {
+
+		$imgSrc = BASE_URL . "inspo/inspo/colors/" . $image;
+
+			echo "<li><picture><img src='$imgSrc'></picture></li>";
+
+	};
+
+	echo "</ul>";
+
+	// Create a picture element for each image
+	// Display elements on the page
+
+}
+
+getInspo();
+
+echo "</main>";
+includeFile('templates/partials/footer.php');
+
