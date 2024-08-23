6c7bd3ee4440e3902a05c685bd8dd8523f156fe4
Jewell Scott <jewelljrscott@gmail.com>
2024-08-22 20:37:51 -0400

    Add forms

--- work/500-proto/add-album.php
+++ work/500-proto/add-album.php
@@ -1,7 +1,10 @@
 <?php 
 
 	if(isset($_POST["add"])) {
-		echo 'Added' . ($_POST["add"]);
+		$newAlbum = $_POST;
+		echo 'Added ' . $newAlbum["title"] . ' by ' . $newAlbum["artist"] . ' (' . $newAlbum["year"] . ')';
+
+		var_dump($newAlbum);
 	}
 
 ?>
@@ -12,25 +15,59 @@
 
 	<fieldset>
 		<label for="">Title</label>
-		<input type="text">
+		<input 
+			name="title" 
+			type="text"
+			required
+			>
 	</fieldset>
 
 	<fieldset>
 		<label for="">Artist</label>
-		<input type="text">
-	</fieldset>
-
-	<fieldset>
-		<label for="">Cover URL</label>
-		<input type="text">
+		<input 
+			name="artist" 
+			type="text"
+			required
+			>
 	</fieldset>
 
 	<fieldset>
 		<label for="">Year</label>
-		<input type="number" min="1900" max="2030" step="1">
+		<input 
+			name="year" 
+			type="number" 
+			value="<?=date("Y")?>"
+			min="1900" 
+			max="2030" 
+			step="1"
+			required
+			>
 	</fieldset>
 
-	<button type="submit" name="add">
+	<fieldset>
+		<label for="">Cover URL</label>
+		<input 
+			name="cover-url" 
+			type="text"
+			pattern="https?://.*\.(?:png|jpg|jpeg)"
+			required
+			>
+	</fieldset>
+
+	<fieldset>
+		<label for="">Description</label>
+		<input 
+			name="description" 
+			type="text"
+			required
+			>
+	</fieldset>
+
+	<button 
+		type="submit" 
+		name="add"
+		required
+		>
 		Add Album
 	</button>
 
