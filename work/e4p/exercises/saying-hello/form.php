cae767dcad920c6e06f0ac6f61258c14b0ad7d02
Jewell Scott <jewelljrscott@gmail.com>
2024-08-31 01:02:17 -0400

    Add Saying Hello

---
+++ work/e4p/exercises/saying-hello/form.php
@@ -0,0 +1,71 @@
+<?php 
+
+	// keep input, string concatenation, and output separate
+
+	$name = "";
+	$greeting = "";
+
+	function generateGreeting($name) {
+		switch (ucfirst($name)) {
+			case "Derek":
+				$greeting = "Ahoy, $name!";
+				break;
+			case "Andy":
+				$greeting = "What's up, $name?";
+				break;
+			case "Anna":
+				$greeting = "Howdy, $name!";
+				break;
+			case "Peter":
+				$greeting = "Salutations, $name!";
+				break;
+			case "Stephanie":
+				$greeting = "Greetings, $name!";
+				break;
+			default:
+				$greeting = "Hello, $name!";
+		}
+		return $greeting;
+		echo $greeting;
+	}
+
+	if(isset($_POST["submit"])) {
+		$name = $_POST["name"];
+
+		$output = generateGreeting($name);
+	}
+
+	// use no variables? maybe later
+	// display different greetings for different people
+
+?>
+
+<form method="POST" class="e4p">
+	<div class="input-wrapper">
+		<fieldset>
+
+			<label for="name">What's your name?</label>
+
+			<input type="text" name="name" required>
+		</fieldset>
+
+		<fieldset>
+			<button class="tech-type" type="submit" name="submit">
+			Submit
+			</button>
+			<button class="tech-type" type="reset" name="reset">
+			<a href="work/e4p/exercises/saying-hello">Reset</a>
+			</button>
+		</fieldset>
+	</div>
+
+	<div class="output-wrapper">
+		  <output name="result" for="name">
+		  		<?php
+					if (isset($_POST["submit"])) {
+						echo $output;
+					}
+				?>
+		  </output>
+	</div>
+</form>
---
+++ work/e4p/exercises/saying-hello/index.php
@@ -0,0 +1,16 @@
+<?php include '../../../../templates/partials/header.php'; ?>
+
+		<main class="page-main">
+			<div class="inner-column">
+				<!-- Add some breadcumbs back to E4P... --> 
+
+				<h1 class="xl-type rainbow">Saying Hello</h1>
+
+				<?php include 'form.php';?>
+			</div>
+		</main>
+
+		<?php include '../../../../templates/partials/footer.php'; ?>
+		</div>
+	</body>
+</html>
