030416c15d88925547eda8cde744e9755830a854
Jewell Scott <jewelljrscott@gmail.com>
2024-09-18 17:26:32 -0400

    Update typography

--- work/layout-garden/modules/type-specimen/template.php
+++ work/layout-garden/modules/type-specimen/template.php
@@ -1,16 +1,56 @@
 <type-specimen>
-	<span class="text-10x">10 - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-9x">9X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-8x">8X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-7x">7X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-6x">6X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-5x">5X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-4x">4X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-3x">3X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-2x">2X - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-xl">XL - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-lg">LG - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-base">BASE - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-sm">SM - Amazingly few discotheques provide jukeboxes</span>
-	<span class="text-xs">XS - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-10x">10x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-9x">9x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-8x">8x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-7x">7x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-6x">6x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-5x">5x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-4x">4x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-3x">3x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-2x">2x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-xl">xl - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-lg">lg - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-base">base - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-sm">sm - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-xs">xs - Amazingly few discotheques provide jukeboxes</span>
+	<br>
+	<span class="text-10x weight-300">10x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-9x weight-300">9x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-8x weight-300">8x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-7x weight-300">7x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-6x weight-300">6x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-5x weight-300">5x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-4x weight-300">4x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-3x weight-300">3x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-2x weight-300">2x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-xl weight-300">xl - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-lg weight-300">lg - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-base weight-300">base - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-sm weight-300">sm - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-xs weight-300">xs - Amazingly few discotheques provide jukeboxes</span>
+	<br>
+	<span class="text-10x weight-800">10x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-9x weight-800">9x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-8x weight-800">8x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-7x weight-800">7x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-6x weight-800">6x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-5x weight-800">5x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-4x weight-800">4x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-3x weight-800">3x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-2x weight-800">2x - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-xl weight-800">xl - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-lg weight-800">lg - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-base weight-800">base - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-sm weight-800">sm - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-xs weight-800">xs - Amazingly few discotheques provide jukeboxes</span>
+	<br>
+	<span class="text-xl text-serif">xl - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-lg text-serif">lg - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-base text-serif">base - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-sm text-serif">sm - Amazingly few discotheques provide jukeboxes</span>
+	<br>
+	<span class="text-xl text-mono">xl - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-lg text-mono">lg - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-base text-mono">base - Amazingly few discotheques provide jukeboxes</span>
+	<span class="text-sm text-mono">sm - Amazingly few discotheques provide jukeboxes</span>
 </type-specimen>
