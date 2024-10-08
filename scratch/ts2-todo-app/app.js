32f851c7f8f9dfd0f03463a62e5763a798e83465
Jewell Scott <jewelljrscott@gmail.com>
2024-10-08 13:44:11 -0400

    Update todo app to use actual ids instead of indexes

--- scratch/ts2-todo-app/app.js
+++ scratch/ts2-todo-app/app.js
@@ -1,6 +1,6 @@
 const households = [
 	{
-		id: 0,
+		id: 1,
 		name: "Lothario",
 		round: 2,
 		subhood: "Pleasantview",
@@ -10,7 +10,7 @@ const households = [
 		notes: "",
 	},
 	{
-		id: 1,
+		id: 2,
 		name: "Goth",
 		round: 2,
 		subhood: "Pleasantview",
@@ -22,7 +22,7 @@ const households = [
 		notes: "Move Morty and Alex in with Dina?",
 	},
 	{
-		id: 2,
+		id: 3,
 		name: "Caliente",
 		round: 2,
 		subhood: "Pleasantview",
@@ -33,7 +33,7 @@ const households = [
 		notes: "If Mortimer moves in, move Nina to the Pleasant Vista Apts",
 	},
 	{
-		id: 3,
+		id: 4,
 		name: "Broke",
 		round: 2,
 		subhood: "Pleasantview",
@@ -46,7 +46,7 @@ const households = [
 		notes: "Brandi just had Bobby Broke and is into Darren",
 	},
 	{
-		id: 4,
+		id: 5,
 		name: "Dreamer",
 		round: 1,
 		subhood: "Pleasantview",
@@ -57,7 +57,7 @@ const households = [
 		notes: "",
 	},
 	{
-		id: 5,
+		id: 6,
 		name: "Pleasant",
 		round: 1,
 		subhood: "Pleasantview",
@@ -73,8 +73,18 @@ const households = [
 
 const original = [...households];
 
-var count = 5;
+var count = 6;
 var currentRound = 1;
+
+// returns the whole object
+function getHouseholdById(id) {
+  return households.find((h) => h.id === id);
+}
+
+// returns the index
+function getIndexById(id) {
+	return households.findIndex((h) => h.id === id);
+}
 
 // just tells me what i did - crud, etc
 function printFeedback(note = "") {
@@ -103,16 +113,16 @@ function printStats() {
 
 function addRound(id, notes = "") {
 	// increment the round
-	households[id].round++;
+	getHouseholdById(id).round++;
 	// add notes
-	households[id].notes = notes;
+	getHouseholdById(id).notes = notes;
 	//output the feedback
-	printFeedback(`Played round ${currentRound} of the ${households[id].name} household.`);
+	printFeedback(`Played round ${currentRound} of the ${getHouseholdById(id).name} household.`);
 	printStats();
 }
 
 // new households start at the current round number
-function createHousehold(name, subhood, notes, ...members) {
+function createHousehold(name, subhood, notes = "", ...members) {
 	var household = {
 		id: ++count,
 		name: name,
@@ -127,18 +137,18 @@ function createHousehold(name, subhood, notes, ...members) {
 
 function updateHousehold(id, key, ...val) {
 	// make a copy so i can ref it if the household ends up deleted
-	let householdCopy = households[id];
+	let householdCopy = getHouseholdById(id);
 
-	households[id][key] = val;
+	getHouseholdById(id)[key] = val;
 	// using dot notation would create a "key" key instead of passing in the argument. COOL!
 
-	printFeedback(`Updated the ${households[id].name} household.`);
-	console.log(households[id]);
+	printFeedback(`Updated the ${getHouseholdById(id).name} household.`);
+	console.log(getHouseholdById(id));
 
 	// if members ends up being 0 (last member dies or moves into a new household), delete the household
 
-	if (households[id].members.length == 0) {
-		printFeedback(`There are no members of the ${households[id].name} household.`);
+	if (getHouseholdById(id).members.length == 0) {
+		printFeedback(`There are no members of the ${getHouseholdById(id).name} household.`);
 		deleteHousehold(id);
 	}
 }
@@ -146,17 +156,21 @@ function updateHousehold(id, key, ...val) {
 
 // deletes the household entirely (if someone moves into a new house, etc);
 function deleteHousehold(id) {
-	let name = households[id].name;
-	households.splice(id, 1);
+	let name = getHouseholdById(id).name;
+	let index = getIndexById(id);
+	households.splice(index, 1);
+
 	printFeedback(`Deleted the ${name} household.`);
 }
 
 
 console.log("Original Households (6)", original);
 
+createHousehold("Langerak", "Pleasantview", "Move in with Daniel (if he moves out?)", "Kaylynn Langerak");
+deleteHousehold(7);
+addRound(5, "Send Dirk to college next round");
+addRound(6, "Mary-Sue caught Daniel cheating. Breakup?");
+updateHousehold(2, "members", "Mortimer Goth", "Cassandra Goth", "Alexander Goth", "Don Lothario");
+updateHousehold(1, "members");
 createHousehold("Burb", "Pleasantview", "Move into town", "John Burb", "Jennifer Burb", "Lucy Burb");
-deleteHousehold(6);
-addRound(4, "Send Dirk to college next round");
-addRound(5, "Mary-Sue caught Daniel cheating. Breakup?");
-updateHousehold(3, "members", "Brandi Broke", "Dustin Broke", "Beau Broke", "Bobbi Broke");
-updateHousehold(0, "members");
+createHousehold("Oldie", "Pleasantview", "Move into town", "Herb Oldie", "Coral Oldie");
