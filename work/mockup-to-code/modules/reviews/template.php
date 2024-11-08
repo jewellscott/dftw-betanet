d3e4ba8eb87c3ee59a7fb151e26c4fe4c2207fd9
Jewell Scott <jewelljrscott@gmail.com>
2024-11-07 19:07:14 -0500

    Scaffold services and reviews

---
+++ work/mockup-to-code/modules/reviews/template.php
@@ -0,0 +1,37 @@
+<reviews>
+	<header>
+		<h2 class="attention-voice">Lorem Ipsum Dolor Sit</h2>
+		<p>Neque, pulvinar vestibulum non aliquam.</p>
+	</header>
+	
+	<ul class="ratings">
+		<li>
+			<div class="stars">
+				* * * * * 
+			</div>
+			<p>“Orci vel eget in eu. Integer amet porttitor hendrerit etiam arcu, aliquet duis pretium consequat. Semper sed viverra enim ut nunc.”</p>
+
+			<author-card>
+				<picture>
+					<img class="profile-icon" src="https://peprojects.dev/images/square.jpg" alt="">
+				</picture>
+				<p>Courtney Henry</p>
+			</author-card>
+			
+		</li>
+		<li>
+			<div class="stars">
+				* * * * * 
+			</div>
+			<p>“Orci vel eget in eu. Integer amet porttitor hendrerit etiam arcu, aliquet duis pretium consequat. Semper sed viverra enim ut nunc.”</p>
+			
+			<author-card>
+				<picture>
+					<img class="profile-icon" src="https://peprojects.dev/images/square.jpg" alt="">
+				</picture>
+				<p>Courtney Henry</p>
+			</author-card>
+		</li>
+	</ul>
+
+</reviews>
---
+++ work/mockup-to-code/modules/services/template.php
@@ -0,0 +1,51 @@
+<services>
+	<header>
+		<h2 class="attention-voice">Etiam nulla lectus molestie at vulputate.</h2>
+		<p>Neque, pulvinar vestibulum non aliquam.</p>
+		<picture>
+			<img src="https://peprojects.dev/images/landscape.jpg" alt="">
+		</picture>
+	</header>
+	<ol class="features">
+		<li>
+			<h3 class="">
+				Sit quam gravida
+			</h3>
+			<p>
+				Et hendrerit turpis habitant cursus semper ultrices in nunc. Erat eget etiam senectus vestibulum risus.
+			</p>
+		</li>
+		<li>
+			<h3 class="">
+				Elementum enim tempus
+			</h3>
+			<p>
+				At tristique elementum metus blandit id sed vel eu a. Diam sed feugiat diam nisi est. Dictumst elit.
+			</p>
+		</li>
+		<li>
+			<h3 class="">
+				Tempor egestas augue
+			</h3>
+			<p>
+				Eu metus, at orci dui sapien. Semper fames est ut gravida. Neque pellentesque risus ut odio.
+			</p>
+		</li>
+		<li>
+			<h3 class="">
+				Nulla ac lacinia
+			</h3>
+			<p>
+				Molestie faucibus amet sed ut gravida iaculis sed venenatis. Pulvinar id lacus condimentum ut. Non.
+			</p>
+		</li>
+		<li>
+			<h3 class="">
+				Velit nibh a
+			</h3>
+			<p>
+				Ac morbi integer orci justo lobortis elementum, massa pulvinar quisque. Porttitor sit convallis quis.
+			</p>
+		</li>
+	</ol>
+</services>
