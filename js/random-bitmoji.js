1108712aff771fd181abc9cfc87b6c0db04f6108
Jewell Scott <jewelljrscott@gmail.com>
2025-01-05 21:24:47 -0500

    Update to include multiple bitmoji replacements

--- js/random-bitmoji.js
+++ js/random-bitmoji.js
@@ -1,6 +1,15 @@
-function getRandomInt(max) { // number between 1 & max
-  return (Math.floor(Math.random() * Math.floor(max)))+1;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
-    
-let jewell = document.getElementById("random-bitmoji");
-jewell.src = "https://peprojects.dev/beta-one/jewell-hosted/bitmoji-resized/numbered/bitmoji" + getRandomInt(174) + ".png";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const jewell = document.querySelector("#random-bitmoji");
+  if (jewell) {
+    jewell.src = "https://peprojects.dev/beta-one/jewell-hosted/bitmoji-resized/numbered/bitmoji" + getRandomInt(174) + ".png";
+  }
+
+  const bitmoji404 = document.querySelector("#bitmoji404");
+  if (bitmoji404) {
+    bitmoji404.src = "https://peprojects.dev/beta-one/jewell-hosted/bitmoji-resized/404/bitmoji" + getRandomInt(48) + ".png";
+  }
+});
