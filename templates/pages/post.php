92b24f50bd913f6da4c457d980143e897134d813
Jewell Scott <jewelljrscott@gmail.com>
2025-01-06 20:58:20 -0500

    Update blog post

--- templates/pages/post.php
+++ templates/pages/post.php
@@ -1,37 +1,40 @@
-<?php 
+<article class="blog-post">
+	<?php 
 
-	$blogFiles = scandir('data/blog');
-    $blogData = [];
+		$blogFiles = scandir('data/blog');
+	    $blogData = [];
 
-    foreach ($blogFiles as $file) {
-        if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
-            // at first it was sending in other files... maybe invisible files
+	    foreach ($blogFiles as $file) {
+	        if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
+	            // at first it was sending in other files... maybe invisible files
 
-            $blog = getData("blog/$file");
+	            $blog = getData("blog/$file");
 
-            array_push($blogData, $blog);
+	            array_push($blogData, $blog);
 
-        }
-    }
+	        }
+	    }
 
-	$pageTitle = $blog["title"];
-	$topLevel = false;
-	$links = $blog["links"];
+		$pageTitle = $blog["title"];
+		$topLevel = false;
+		$links = $blog["links"];
 
-	$pageDescription = $blog["description"];
+		$pageDescription = $blog["description"];
+		$lastUpdated = $blog["lastUpdated"];
+		$published = $blog["published"];
 
-	include('templates/modules/page-header/template.php'); 
 
-?>
+		include('templates/modules/page-header/template.php'); 
 
-<?php foreach ($blog["module"] as $module) { ?>
-	<section>
-		<inner-column>
-			<?php 
-				
-				renderModule($module["module"], $module["props"]);
-			 ?>
-		</inner-column>
-	</section>
-<?php } ?>
+	?>
 
+	<?php foreach ($blog["modules"] as $module) { ?>
+		<section>
+			<inner-column>
+				<?php 
+					renderModule($module["module"], $module["props"]);
+				 ?>
+			</inner-column>
+		</section>
+	<?php } ?>
+</article>
