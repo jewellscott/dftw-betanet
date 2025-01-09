65af93359c8b3ffa70f677919abbbe3cfd100989
Jewell Scott <jewelljrscott@gmail.com>
2025-01-08 21:28:50 -0500

    Fix project routing

--- templates/pages/project.php
+++ templates/pages/project.php
@@ -3,6 +3,8 @@
 	$projectFiles = scandir('data/projects');
     $projects = [];
 
+    $thisProject = null;
+
     foreach ($projectFiles as $file) {
         if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
             // at first it was sending in other files... maybe invisible files
@@ -16,14 +18,22 @@
 
 	$slug = $_GET["slug"] ?? null; 
 
+	// if this slug actually exists in a json file in the subdirectory, THEN load the header
+
+
 		foreach ($projectFiles as $file) {
 			if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
 			   $project = getData("projects/$file");
 			   
 			   if (isset($project['slug']) && $project['slug'] === $slug) {
 
-			   	// if this slug actually exists in a json file in the subdirectory, THEN load the header
 
+			   		// set the right project to this project
+
+			   		$thisProject = $project;
+
+
+			   		// include the header now that the file exists (so you don't see the header if 404 page is included)
 
 					$pageTitle = $project["title"];
 					$topLevel = false;
@@ -36,10 +46,11 @@
 			}
 		}
 
-
 ?>
 
-<?php foreach ($project["case-study"] as $module) { ?>
+<?php foreach ($thisProject["case-study"] as $module)
+
+ { ?>
 	<section>
 		<inner-column>
 			<?php 
