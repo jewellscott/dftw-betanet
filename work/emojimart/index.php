6de05746bee9de245b8aa4dc19c5580dca3a1964
Jewell Scott <jewelljrscott@gmail.com>
2025-01-23 15:58:14 -0500

    Update emojimart buttons

--- work/emojimart/index.php
+++ work/emojimart/index.php
@@ -5,6 +5,8 @@
 	$json = file_get_contents('data/products.json');
 	$productData = json_decode($json, true); 
 
+	$cart = [];
+
  ?>
 
  <emojimart-app>
@@ -23,7 +25,7 @@
 	 		<?php foreach ($category["products"] as $product) { ?>
 
 	 			<li class="<?=$product["id"]?>">
-	 				<product-card>
+	 				<product-card data-id="<?=$product["id"]?>">
 	 					<picture>
 	 						<img 
 	 							src="<?=$product["thumb"]?>" 
@@ -33,7 +35,15 @@
 	 					<p class="name"><?=$product["name"]?></p>
 	 					<p><span class="price">$<?=$product["price"]?></span> <span class="unit">/<?=$product["unit"]?></span></p>
 
-	 					<button>+</button>
+	 					<form action="" method="POST" class="product-actions">
+							<button type="submit" name="action" value="decrease" class="btn-decrease">-</button>
+
+							<span class="count">
+							   0
+							</span>
+
+							<button type="submit" name="action" value="increase" class="btn-increase">+</button>
+						</form>
 	 				</product-card>
 	 			</li>
 
@@ -51,15 +61,21 @@
 
  <checkout-view>
 
- 	<empty-cart>
+ 	<?php if (!empty($cart)) { ?>
+ 		<!-- display cart -->
+ 	<?php } else { ?>
 
- 		<p>Your cart is empty.<br> Please enjoy a landscape.</p>
+ 		<empty-cart>
 
-	 	<picture>
-	 		<img src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/national-park.png" alt="National Park Emoji">
-	 	</picture>
+	 		<p>Your cart is empty.<br> Please enjoy a landscape.</p>
+
+		 	<picture>
+		 		<img src="https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/national-park.png" alt="National Park Emoji">
+		 	</picture>
  		
- 	</empty-cart>
+ 		</empty-cart>
+
+ 	<?php } ?>
 
  </checkout-view>
 
