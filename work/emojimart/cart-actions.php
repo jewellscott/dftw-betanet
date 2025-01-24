2a2ee322de906ead8701ffa794523d9443028d13
Jewell Scott <jewelljrscott@gmail.com>
2025-01-23 19:11:59 -0500

    Document cart-actions

--- work/emojimart/cart-actions.php
+++ work/emojimart/cart-actions.php
@@ -3,24 +3,23 @@
 // session_start();
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
-
-// echo "<pre>";
 // var_dump($_SESSION);
 // var_dump($_POST);
-// die(); // Stop execution here
-
-// $productId = $_POST['product_id'];
+// die(); 
 
 session_start(); 
+// start a new session, get the cookie if it exists
 
 $productId = $_POST['product_id'];
 
 if ($_POST['action'] === 'increase') {
+    // if the product's not there, add it and set it to 0 to start with
     if (!isset($_SESSION['cart'][$productId])) {
         $_SESSION['cart'][$productId] = 0;
     }
     $_SESSION['cart'][$productId]++;
 } 
+
 elseif ($_POST['action'] === 'decrease') {
     if (isset($_SESSION['cart'][$productId])) {
         if ($_SESSION['cart'][$productId] <= 1) {
@@ -33,9 +32,7 @@ elseif ($_POST['action'] === 'decrease') {
 }
 
 
-// var_dump("Final cart state:", $_SESSION['cart']); 
-// die();
-
+// after the server updates the cart, go back to home
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 exit();
 
