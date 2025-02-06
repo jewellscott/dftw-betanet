9fad506a5feab6292c854666951d71e2a4aa4419
Jewell Scott <jewelljrscott@gmail.com>
2025-02-06 11:58:20 -0500

    Update project card

--- templates/components/project-card/template.php
+++ templates/components/project-card/template.php
@@ -5,6 +5,8 @@
 	$teaser = $project["teaser"];
 	$description = $project["description"];
 	$slug = $project["slug"];
+	$slug = $project["slug"];
+	$github = $project["links"][1];
 
  ?>
 
@@ -15,7 +17,10 @@
 	<card-content>
 		<h3 class="lg-type strong-type colored-type"><?=$title?></h3>
 		<p class="display-type"><?=$teaser?></p>
+		<p class="display-type"><?=$description?></p>
+
 		<!-- <a href="<?="?page=" . $url?>" class="tech-type">View <?=$title?> live</a> -->
-		<a href="?page=project&slug=<?=$slug?>" class="tech-type">Case Study</a>
+		<a href="?page=project&slug=<?=$slug?>" class="tech-type">Read Case Study</a>
+		<!-- <a href="<?=$github?>" class="tech-type">Browse on Github</a> -->
 	</card-content>
 </project-card>
