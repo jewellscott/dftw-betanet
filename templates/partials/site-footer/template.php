238558868948652957b3e947a15cc8292c2a13f5
Jewell Scott <jewelljrscott@gmail.com>
2025-02-05 20:56:40 -0500

    Remove footer contact

--- templates/partials/site-footer/template.php
+++ templates/partials/site-footer/template.php
@@ -2,13 +2,6 @@
 
 <footer class="site-footer">
 		<p class="tagline tech-type"><a href="https://youtu.be/R_nviNS0IAw?si=G_oLg3MDTul68Zj5&t=205" target="_blank" id="computer-blue">Poor, lonely computer. It's time someone programmed you.</a></p>
-		<nav class="contact-nav">
-			<ul>
-				<li><a href="/">Contact</a></li>
-				<span class="footer-icon"> / </span>
-				<li><a href="/">Substack</a></li>
-			</ul>
-		</nav>
 	</footer>
 
 	<script src="js/app.js" type="module"></script>
